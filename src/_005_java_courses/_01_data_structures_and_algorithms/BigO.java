ebc6fba7f0fe11e4c9d701895989cc3ddb0ea50b
Adrian <adrian.talonia11@gmail.com>
2024-02-01 22:31:13 -0600

    add data strauctures and algorithms course

---
+++ src/_005_java_courses/_01_data_structures_and_algorithms/BigO.java
@@ -0,0 +1,28 @@
+package _005_java_courses._01_data_structures_and_algorithms;
+
+public class BigO {
+
+    public static void main(String[] args) {
+
+        printItemsLinear(10);
+        printItemsQuadratic(10);
+    }
+
+    public static void printItemsLinear(int i) {
+
+        // Big O(n) = Linear Time increases with the size of the input
+        for (int j = 0; j < i; j++) {
+            System.out.println(j);
+        }
+    }
+
+    public static void printItemsQuadratic(int n) {
+
+        // Big O(n^2) = Quadratic time grows with the size of the input
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println(i + " " + j);
+            }
+        }
+    }
+}
---
+++ src/_005_java_courses/_01_data_structures_and_algorithms/README.md
@@ -0,0 +1,3 @@
+# Java Data Structures & Algorithms + LEETCODE Exercises
+
+DSA for the Cracking the Coding Interview. Animated Examples for Faster Learning and Deeper Understanding.
