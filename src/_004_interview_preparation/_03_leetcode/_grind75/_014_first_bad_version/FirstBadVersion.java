c7bb3ffa42888f58e667dcf4168e73eb5eac6fbf
Adrian <adrian.talonia11@gmail.com>
2024-02-06 22:45:00 -0600

    problem number 13

---
+++ src/_004_interview_preparation/_03_leetcode/_grind75/_014_first_bad_version/FirstBadVersion.java
@@ -0,0 +1,13 @@
+package _004_interview_preparation._03_leetcode._grind75._014_first_bad_version;
+
+public class FirstBadVersion {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int firstBadVersion(int n) {
+
+        return 0;
+    }
+}
