387b9a6839182b21cdee1322c4cd808b616721e3
Adrian <adrian.talonia11@gmail.com>
2024-04-29 22:28:19 -0600

    single linked list push item at the front

--- src/_005_java_courses/_01_data_structures_and_algorithms/_03_linked_lists/_01_singly_l0inked_list/LinkedList.java
+++ src/_005_java_courses/_01_data_structures_and_algorithms/_03_linked_lists/_01_singly_l0inked_list/LinkedList.java
@@ -5,31 +5,56 @@ public class LinkedList {
 
     Node head;
 
-    class Node {
-        int data;
-        Node next;
-
-        Node(int data) {
-            data = data;
-            next = null;
-        }
-    }
-
     /* This function is in LinkedList class. Inserts a
    new Node at front of the list. This method is
    defined inside LinkedList class shown above */
 
     public void push(int new_data)
     {
-    /* 1 & 2: Allocate the Node &
-              Put in the data*/
-        Node new_node = new Node(new_data);
+        // 1. allocate node
+        Node new_node = new Node();
 
-        /* 3. Make next of new Node as head */
+        // 2. put in the data
+        new_node.data = new_data;
+
+        // 3. Make next of new node as head
         new_node.next = head;
 
-        /* 4. Move the head to point to new Node */
+        // 4. move the head to point
+        // to the new node
         head = new_node;
     }
 
+    void printList()
+    {
+        Node node = head;
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Start with the empty list
+        LinkedList list = new LinkedList();
+
+        list.push(6);
+        list.push(5);
+        list.push(4);
+        list.push(3);
+        list.push(2);
+        list.push(1);
+
+        System.out.print(
+                "After inserting nodes at their front: ");
+        list.printList();
+
+    }
+
+}
+
+class Node {
+    int data;
+    Node next;
 }
--- src/_005_java_courses/_01_data_structures_and_algorithms/_03_linked_lists/_01_singly_l0inked_list/README.md
+++ src/_005_java_courses/_01_data_structures_and_algorithms/_03_linked_lists/_01_singly_l0inked_list/README.md
@@ -23,3 +23,44 @@ class LinkedList {
     }
 }
 ```
+
+Given a Linked List, the task is to insert a new node in this given Linked List at the following positions:
+
+- At the front of the linked list  
+- After a given node.
+- At the end of the linked list.
+
+## How to Insert a Node at the Front/Beginning of Linked List
+
+**Approach**:
+
+To insert a node at the start/beginning/front of a Linked List, we need to:
+
+1) Make the first node of Linked List linked to the new node
+2) Remove the head from the original first node of Linked List
+3) Make the new node as the Head of the Linked List.
+
+![image](https://media.geeksforgeeks.org/wp-content/uploads/20240222162726/Insertion-at-the-Beginning-of-Singly-Linked-List.webp)
+
+```java
+/* This function is in LinkedList class. Inserts a
+   new Node at front of the list. This method is
+   defined inside LinkedList class shown above */
+public void push(int new_data)
+{
+    /* 1 & 2: Allocate the Node &
+              Put in the data*/
+    Node new_node = new Node(new_data);
+ 
+    /* 3. Make next of new Node as head */
+    new_node.next = head;
+ 
+    /* 4. Move the head to point to new Node */
+    head = new_node;
+}
+```
+
+## Complexity Analysis:
+
+- Time Complexity: O(1), We have a pointer to the head and we can directly attach a node and change the pointer. So the Time complexity of inserting a node at the head position is O(1) as it does a constant amount of work.
+- Auxiliary Space: O(1)
